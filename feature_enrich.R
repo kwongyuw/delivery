1531bb080eabbb8cf2149da41341f5b3edb6541b
Kwong-Yu Wong <kwongyuw@uw.edu>
2019-01-22 14:26:01 -0800

    added user/rider/sup experience in handling orders

--- feature_enrich.R
+++ feature_enrich.R
@@ -12,9 +12,24 @@ df <- read_xlsx(data_fn)
 #features wanted:
 #(from raw) require_tm, price, delivery fee, sup_id
 #(from API) dest neighborhood, sup neighborhood
-#(manip raw) user frequency (i.e. how familiar w/ the app), rider frequency (i.e. how experienced is rider)
+#(manip raw) order features
 #            store frequency (i.e. how experience is restaurant), supplier volume by time
-#            order features
+#            user frequency (i.e. how familiar w/ the app), rider frequency (i.e. how experienced is rider)
+
+
+#(manip raw) order features
+#timing info
+df <- mutate(df, delay=as.numeric(finish_tm)-as.numeric(require_tm), prereq=as.numeric(require_tm)-as.numeric(place_tm), 
+             prepare=as.numeric(leave_tm)-as.numeric(receive_tm), ride=as.numeric(finish_tm)-as.numeric(leave_tm), 
+             allow_cook=prereq-ride, #cooka=as.numeric(cookable_tm)-as.numeric(cook_tm), 
+             left1=prereq-(prepare+ride)) #left3=prereq-(dish_longer*60 + ride), #left2=prereq-(cooka+ride), 
+
+
+#weather when placing order (non-run-able)
+pcp_dur=as.numeric(place_tmref)-as.numeric(pcp_beginref) + as.numeric(as.Date(place_tm)-as.Date(shw_begin))*24, 
+shw_dur=as.numeric(place_tmref)-as.numeric(shw_beginref) + as.numeric(as.Date(place_tm)-as.Date(shw_begin))*24,
+rel_tm=ymd_h(ifelse(hour(place_tm)>=6, (paste(date(place_tm),6)), (paste((date(place_tm)-1),6)))), 
+shw_rdur=pmin(shw_dur,(hour(place_tm)-hour(rel_tm))+(minute(place_tm)-minute(rel_tm))/60)) #shw_rel should always be + , shw_rdur1=pmin(shw_dur,as.numeric(require_tm)-as.numeric(rel_tm))
 
 #(manip raw)rider frequency (i.e. how experienced is rider)
 #worker-order ratio (#workers active, #orders to be delivered every 30mins)
@@ -68,44 +83,36 @@ df <- left_join(df,roster30, by = c("require_date"="date", "require_tmref"="tmre
   left_join(order30, by=c("require_date"="read_date", "require_tmref")) %>%
   mutate(ow_ratio = order_n/avai_n30, tm=NULL)
 
-#Number of orders finished in each shift
-worker <- group_by(df, rider_id, as.Date(read_tm)) %>%
-  arrange(read_tm) %>%
-  mutate(new_shift=ifelse(is.na(lag(finish_tm)),FALSE,((as.numeric(read_tm)-as.numeric(lag(finish_tm)))/3600)>1), shift=cumsum(new_shift))
-worker <- group_by(worker) %>% group_by(rider_id, as.Date(read_tm), shift) %>% 
-  summarize(finished_n=n())
-
-
-
-
-
-
-
-
-
-
-
 #(manip raw)user frequency (i.e. how familiar w/ the app)
-dfl_user <- group_by(dfl, user_id) %>%
-  arrange(user_id, finish_tm) %>%
-  summarize(n=n(), delay_avg=mean(delay), dbad_avg=mean(day_bad), tmax_avg = mean(temp_max),
-            rinc_avg=mean(rider_income), lunch=as.factor(mean(hour(finish_tm))>10 & mean(hour(finish_tm))<14), prereq_avg = mean(require_tm - place_tm), 
-            price_avg=mean(price), price_sd=sd(price), span=max(finish_tm)-min(finish_tm))
-dfl <- left_join(dfl, dfl_user, by="user_id")
-
+df <- arrange(df, user_id, place_tm) %>% group_by(user_id) %>% mutate(count=1, user_exp=cumsum(count)) %>% group_by()
+#rider experience (in # orders finished under data period)
+df <- arrange(df, rider_id, read_tm) %>% group_by(rider_id) %>% mutate(rider_exp=cumsum(count)) %>% group_by()
 #(manip raw) store frequency (i.e. how experience is restaurant), supplier volume by time
+df <- arrange(df, sup_id, read_tm) %>% group_by(sup_id) %>% mutate(sup_exp=cumsum(count)) %>% group_by()
 
 
-#(manip raw) order features
-#timing info
-dfl <- mutate(dfl, delay=as.numeric(finish_tm)-as.numeric(require_tm), prereq=as.numeric(require_tm)-as.numeric(place_tm), 
-              prepare=as.numeric(leave_tm)-as.numeric(receive_tm), ride=as.numeric(finish_tm)-as.numeric(leave_tm), 
-              allow_cook=prereq-ride, #cooka=as.numeric(cookable_tm)-as.numeric(cook_tm), 
-              left1=prereq-(prepare+ride)) #left3=prereq-(dish_longer*60 + ride), #left2=prereq-(cooka+ride), 
+
+#Number of orders finished in each shift
+df <- group_by(df, rider_id, read_date=as.Date(read_tm)) %>%
+  arrange(read_tm) %>%
+  mutate(new_shift=ifelse(is.na(lag(finish_tm)),FALSE,((as.numeric(read_tm)-as.numeric(lag(finish_tm)))/3600)>1), shift=cumsum(new_shift)) %>%
+  group_by()
+worker <- group_by(df) %>% group_by(rider_id, read_date, shift) %>% 
+  summarize(finished_n=n()) %>%
+  arrange(rider_id, read_date, shift) %>%
+  group_by(rider_id) %>%
+  mutate(exp_n=cumsum(finished_n))
+
+df <- left_join(df, worker, by = c("rider_id", "read_date", "shift"))
 
 
-#weather when placing order (non-run-able)
-              pcp_dur=as.numeric(place_tmref)-as.numeric(pcp_beginref) + as.numeric(as.Date(place_tm)-as.Date(shw_begin))*24, 
-              shw_dur=as.numeric(place_tmref)-as.numeric(shw_beginref) + as.numeric(as.Date(place_tm)-as.Date(shw_begin))*24,
-              rel_tm=ymd_h(ifelse(hour(place_tm)>=6, (paste(date(place_tm),6)), (paste((date(place_tm)-1),6)))), 
-              shw_rdur=pmin(shw_dur,(hour(place_tm)-hour(rel_tm))+(minute(place_tm)-minute(rel_tm))/60)) #shw_rel should always be + , shw_rdur1=pmin(shw_dur,as.numeric(require_tm)-as.numeric(rel_tm))
+#user features
+user <- group_by(df, user_id) %>%
+  arrange(user_id, finish_tm) %>%
+  summarize(n=n(), rinc_avg=mean(rider_income), #, delay_avg=mean(delay)
+            lunch=as.factor(mean(hour(finish_tm))>10 & mean(hour(finish_tm))<14), prereq_avg = mean(require_tm - place_tm), 
+            price_avg=mean(price), price_sd=sd(price), span=max(finish_tm)-min(finish_tm))
+
+
+
+
