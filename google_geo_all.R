11b62c6a0668659368242eeece8056d24b4fb602
jmhernan <jmhernandeze@gmail.com>
2019-01-24 12:17:34 -0800

    whole data geo run

--- google_geo_all.R
+++ google_geo_all.R
@@ -33,14 +33,6 @@ geo_data <- data %>%
   select(address) %>% 
   distinct(.)
 
-# test <- data %>%
-#   group_by(address) %>%
-#   summarise(n = n()) %>%
-#   mutate(ad_id = 1:nrow(.))
-# 
-# test2 <-left_join(data, test %>%
-#                           select(-n), by = c("address"))
-##
 
 #Baidu API call
 geocodeAddress <- function(address) {
@@ -58,23 +50,6 @@ geocodeAddress <- function(address) {
   Sys.sleep(0.0167) # Verified developer now 160/s (API only allows 50 requests per second for non-verified user)
   out
 }
-#Google API call
-#geocodeAddress <- function(address) {
-#  require(RJSONIO)
-#  full <- paste(address)
-#  url <- "https://maps.google.com/maps/api/geocode/json?address="
-#  url <- URLencode(paste(url, full, '&sensor=false&key=',apikey, sep = ""))
-#  x <- fromJSON(url, simplify = FALSE)
-#  if (x$status == "OK") {
-#    
-#      out <- c(x$results[[1]]$geometry$location$lat,
-#               x$results[[1]]$geometry$location$lng)
-#    } else {
-#      out <- NA
-#  }
-#  Sys.sleep(0.05)  # API only allows 50 requests per second
-#  out
-#}
 
 #Initialize
 geo_data$LAT<-NA
@@ -108,4 +83,55 @@ data_geo_aug1 <- bind_cols(Aug_C3_P1,geo_test)
 #append to overall
 data_geo_all <- bind_rows(data_result, data_geo_aug1)
 
+# save data 
+# write.csv() 
 
+
+###########
+# run from_address 
+##########
+rm(list=setdiff(ls(), c("data_geo_all", "ak_bd_brow", "ak_bd_serv", "apikey")))
+
+data_geo_all <- data %>% 
+  slice(1:20) #TEST
+
+geo_data <- data_geo_all %>%
+  select(from_addr) %>% 
+  distinct(.)
+
+
+#Baidu API call
+geocodeAddress <- function(address) {
+  require(RJSONIO)
+  full <- paste(address)
+  url <- "http://api.map.baidu.com/geocoder/v2/?address="
+  url <- URLencode(paste(url, full, '&output=json&ak=', ak_bd_brow, sep=''))
+  x <- fromJSON(url,simplify=FALSE)
+  if (x$status == 0) {
+    out <- c(x$result$location$lat, 
+             x$result$location$lng)
+  } else {
+    out <- NA
+  }
+  Sys.sleep(0.0167) # Verified developer now 160/s (API only allows 50 requests per second for non-verified user)
+  out
+}
+
+#Initialize
+geo_data$LAT<-NA
+geo_data$LON<-NA
+g_add=list()
+
+for (i in 1:nrow(geo_data)) {
+  g_add <- geocodeAddress(geo_data$address[i])
+  geo_data$LAT[i] <- g_add[1]
+  geo_data$LON[i] <- g_add[2]
+  
+  if ((i %% 100) ==0) cat(paste(i," "))
+  if (i == nrow(geo_data)) cat("Done!\n")
+}
+
+#add addresses w/geo_cords back to main dataset
+data_result <- left_join(data_geo_all, geo_data, by = c("from_addr"))
+
+# write.csv()
