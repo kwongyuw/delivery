b96319eeec7ece3bfcaecfd83482c1b667602207
jmhernan <jmhernandeze@gmail.com>
2019-02-14 15:21:53 -0800

    control group development

--- control_groups.R
+++ control_groups.R
@@ -15,12 +15,6 @@ temp = list.files(path = data_dir, pattern="*.xlsx$")
 or_df <- do.call(bind_rows,
         lapply(file.path(data_dir ,temp), read_excel, range = cell_cols(1:21)))
 
-# Data with lat and long for both destination and origin 
-# f_df <- fread(file = file.path(data_dir,'data_full_from_addr.csv'), 
-#                 na.strings = c(""),
-#                 stringsAsFactors = F,
-#                 header = T)
-
 # Load everything that has the names dist and append 
 temp = list.files(path = data_dir, pattern="dist")
 temp
@@ -28,15 +22,21 @@ temp
 dist_df <- do.call(bind_rows,
   lapply(file.path(data_dir ,temp), read.csv, stringsAsFactors = FALSE)) %>%
   unique(.)
-names(f_df)
+
+
 # Join to main data using "id" 
 a_df <- left_join(or_df, dist_df, by = "id")
 
-
-# join restaurant information 
+# restaurant information 
 sup <- read.csv(file.path(data_dir ,'sup.csv'), stringsAsFactors = F) %>% select(-X.1,-X)
 
+# join
 df_all <- left_join(a_df, sup, by=c('sup_id', 'from_tel', 'from_addr'))
 
+# features 
+df_feat <- read.csv(file.path(data_dir ,'data_derived.csv'), stringsAsFactors = F)
+
+#join
+df_all <- left_join(df_all , df_feat, by='id')
 
 
