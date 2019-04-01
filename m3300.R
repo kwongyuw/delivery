bfe2a928614d2c7e1d90993ef0f9c4be1b767a07
Kwong-Yu Wong <kwongyuw@uw.edu>
2019-04-01 09:40:46 -0700

    seems no rider has ever touched beyond 000 per month

--- m3300.R
+++ m3300.R
@@ -3,8 +3,10 @@ library(stargazer)
 
 source('/Users/kwongyu/Google Drive/dwb/git/delivery/dataprocess_model.R')
 
-rider <- group_by(df) %>% group_by(rider_id, read_date, r_shift) %>% 
-  summarize(r_inshift_n=n()) %>%
-  arrange(rider_id, read_date, r_shift) %>%
-  group_by(rider_id) %>%
-  mutate(r_exp_n=cumsum(r_inshift_n))
+rider_m <- group_by(crt_df, rider_id, month = month(finish_tm)) %>% 
+  summarise(orig_inc = sum(rider_org_income), inc = sum(rider_income))
+
+ggplot(filter(rider_m, inc/100 > 10), aes(x=inc/100)) + 
+  stat_bin(binwidth = 10, alpha=0.1) 
+
+plot(as.factor(rider_m$month), rider_m$inc/100, cex=0.5)
