682580759977dafb2df9be13c6712c73022ff55c
Kwong-Yu Wong <kwongyuw@uw.edu>
2019-04-14 17:49:18 -0700

    speed or complication don't seem to show diff across the threshold

--- m3300.R
+++ m3300.R
@@ -3,26 +3,50 @@ library(stargazer)
 
 source('/Users/kwongyu/Google Drive/dwb/git/delivery/dataprocess_model.R')
 
-# if use crt_df (after cleaning) only 142 ppl left
-rider_m <- mutate(crt_df, roi_subs=rider_org_income+300, ri_subs=rider_income+300) %>%
-  group_by(rider_id, month = month(finish_tm)) %>%
-  summarise(orig_inc = sum(rider_org_income), inc = sum(rider_income), 
-            orig_inc_subs=sum(roi_subs), inc_subs=sum(ri_subs),
-            num=n())
-ggplot(filter(rider_m, inc/100 > 10), aes(x=inc/100)) + 
-  stat_bin(binwidth = 10, alpha=0.1) 
-plot(as.factor(rider_m$month), rider_m$inc/100, cex=0.5)
+df <- group_by(df_all, rider_id, month=month(finish_tm)) %>%
+  select(rider_id, month, ends_with("_tm"), rider_income, dist, time) %>%
+  mutate(ri_subs=rider_income+300,  count_atm=order(order(finish_tm)), 
+         inc_atm=cumsum(rider_income),sinc_atm=cumsum(ri_subs),
+         excel_month=(max(inc_atm)/100>3000), sexcel_month=(max(sinc_atm)/100>3000), 
+         speed = dist/ifelse(finish_tm!=leave_tm, as.numeric(finish_tm - leave_tm), NA),
+         bdunexp = ifelse(finish_tm!=leave_tm, as.numeric(finish_tm - leave_tm), NA)/time) %>%
+  filter(dist<14000, speed<30, time>0)
+
+summary(filter(df, inc_atm/100<=3000)$speed)
+summary(filter(df, inc_atm/100>3000)$speed)
+
+# Exceling month X before/after $3000
+## speed
+summary(filter(df, !excel_month, inc_atm/100<3000)$speed)
+summary(filter(df, excel_month, inc_atm/100<3000)$speed)
+summary(filter(df, excel_month, inc_atm/100>=3000)$speed)
+table(df$excel_month, (df$inc_atm/100>=3000))
+summary(filter(df, !sexcel_month, sinc_atm/100<3000)$speed)
+summary(filter(df, sexcel_month, sinc_atm/100<3000)$speed)
+summary(filter(df, sexcel_month, sinc_atm/100>=3000)$speed)
+table(df$sexcel_month, (df$sinc_atm/100>=3000))
+## complication in reality (deliver time vs baidu-map expected time)
+summary(filter(df, !excel_month, inc_atm/100<3000)$bdunexp)
+summary(filter(df, excel_month, inc_atm/100<3000)$bdunexp)
+summary(filter(df, excel_month, inc_atm/100>=3000)$bdunexp)
+table(df$excel_month, (df$inc_atm/100>=3000))
+summary(filter(df, !sexcel_month, sinc_atm/100<3000)$bdunexp)
+summary(filter(df, sexcel_month, sinc_atm/100<3000)$bdunexp)
+summary(filter(df, sexcel_month, sinc_atm/100>=3000)$bdunexp)
+table(df$sexcel_month, (df$sinc_atm/100>=3000))
 
 
-# if use df_all (no cleaning), 866 riders
-rider_ma <- mutate(df_all, roi_subs=rider_org_income+300, ri_subs=rider_income+300) %>%
-  group_by(rider_id, month = month(finish_tm)) %>%
-  summarise(orig_inc = sum(rider_org_income), inc = sum(rider_income), 
-            orig_inc_subs=sum(roi_subs), inc_subs=sum(ri_subs),
-            num=n())
+
+# if use df_all (no cleaning), 866 riders >$3000
+rider_ma <- mutate(df, ri_subs=rider_income+300) %>%
+  group_by(rider_id, month) %>%
+  summarise(inc = sum(rider_income), inc_subs=sum(ri_subs),
+            num=n(), speed_avg=mean(speed, na.rm=TRUE), speed_sd=sd(speed, na.rm=TRUE))
+summary(filter(rider_ma, inc_subs/100<=3000)$speed_avg)
+summary(filter(rider_ma, inc_subs/100>3000)$speed_avg)
+
 
 ggplot(filter(rider_ma, inc/100 > 10), aes(x=inc_subs/100, fill=month)) + geom_histogram(binwidth = 5)
 plot(as.factor(rider_ma$month), rider_ma$inc_subs/100, cex=0.5)
 
 plus3ka <- filter(rider_ma,inc_subs/100>3000)
-
