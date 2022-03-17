456c9db540bb2317e12e647d5ec978d30ee7e952
Kwong-Yu Wong <kwongyuw@uw.edu>
2022-03-16 18:03:31 -0700

    set the dwb line

--- analysis.R
+++ analysis.R
@@ -4,6 +4,7 @@ library(tidyverse)
 # detach("package:tidyverse", unload=TRUE)
 library(stargazer)
 #source('~/Documents/eScience/projects/delivery/dataprocess_model.R') # data from control groups
+setwd("/Users/kwongyu/OneDrive - UW/Projects/dwb")
 source('git/delivery/dataprocess_model.R')
 
 library(RCurl)
