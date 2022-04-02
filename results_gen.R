9e0838f5370e0ba63e748b223aa01dbf3bc734ea
Kwong-Yu Wong <kwongyuw@uw.edu>
2022-04-02 11:30:02 -0700

    add file to gen results. descriptive stat

---
+++ results_gen.R
@@ -0,0 +1,10 @@
+library(caret)
+library(tidyverse)
+# detach("package:tidyverse", unload=TRUE)
+library(stargazer)
+library(psych)
+#source('~/Documents/eScience/projects/delivery/dataprocess_model.R') # data from control groups
+setwd("/Users/kwongyu/OneDrive - UW/Projects/dwb")
+source('git/delivery/dataprocess_model.R')
+
+# Descriptive statistics ####
